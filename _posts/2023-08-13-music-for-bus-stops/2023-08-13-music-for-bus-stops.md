bbfcc6e898da245bfc4872290a608404113c3cef
Bobby <bobby@returnofthemac.lan>
2023-08-15 15:51:58 -0700

    Music For Bus Stops

---
+++ _posts/2023-08-13-music-for-bus-stops/2023-08-13-music-for-bus-stops.md
@@ -0,0 +1,24 @@
+---
+layout: 'p5js'
+title:  "Music for Bus Stops"
+date:   2023-08-13 10:31:00 -0800
+summary: "Algorithmic Ambience on the Cheap"
+custom_css: sound
+tags:
+- javascript
+- music
+- procedural generation
+---
+
+<div id='song-container'>
+  <div class='title-box'>
+    <div class='title'>AMBIENT <span>⬢</span></div>
+    <div class='subtitle'>MUSIC FOR BUS STOPS</div>
+    <div class='name'>BOOTLEGGED BY BOBBY MEYER</div>
+  </div>
+
+
+</div>
+
+
+<script src='sound.js'></script>
---
+++ _posts/2023-08-13-music-for-bus-stops/sound.js
@@ -0,0 +1,89 @@
+console.log('lowrider.js');
+
+let samples = [];
+let ffts = [];
+let playing;
+let r = 0;
+
+function preload() {
+  soundFormats('mp3');
+  i = 1;
+  while (i <= 8) {
+    samples.push(loadSound('samples/p' + i + '.mp3'));
+    i++;
+  }
+  bg = loadImage('map.png');
+}
+
+function setup() {
+  angleMode(DEGREES);
+  playing = false;
+
+  cnv = createCanvas(600, 600);
+  cnv.parent('song-container');
+  cnv.mousePressed(toggleMusic);
+
+  for(sample of samples) {
+    fft = new p5.FFT();
+    fft.setInput(sample);
+    ffts.push(fft);
+  };
+  console.log(ffts);
+}
+
+function toggleMusic() {
+  playing = !playing;
+  console.log(playing);
+  if (playing == true) {
+    playMusic();
+  }
+}
+
+async function playMusic() {
+  while (playing == true) {
+    playSample();
+    waitTime = random(4, 10) * 1000;
+    await new Promise(r => setTimeout(r, waitTime));
+  }
+}
+
+function playSample() {
+  sampleIndex = Math.floor(random(0, samples.length - 1));
+  console.log(sampleIndex);
+  samples[sampleIndex].play(0, 0.5, 1, 0, samples[sampleIndex].duration());
+}
+
+
+function draw() {
+  fill(230, 220, 200);
+  push();
+  tint(255, 127);
+  image(bg, 0, 0, 600, 600);
+  pop();
+  strokeWeight(0);
+  rect(0, 10, 600, 120);
+  textSize(48);
+  fill(230, 220, 200);
+  rect(545, 545, 160, 160);
+  fill(0);
+  text(playing == true ? '⏸' : '⏵', 560, 590);
+
+  noFill();
+
+  strokeWeight(1);
+  translate(250, 20);
+  for (fft of ffts) {
+
+    translate(0, 11);
+    let spectrum = fft.analyze();
+    beginShape();
+    h = 0;
+    for (i = 0; i < spectrum.length; i++) {
+      vertex(i * 2, map(spectrum[i]/50, h, width, 0, height));
+    }
+
+    endShape();
+  }
+}
+
+// http://localhost:4000/2023/08/13/music-for-bus-stations.html
